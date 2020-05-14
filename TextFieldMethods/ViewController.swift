1a2e5e26bbfa0db314cb626dcbb839cbe00ddd1a
Penchal <vemireddy.penchalreddy>
2020-05-14 08:10:27 +0530

    Initial Commit

---
+++ TextFieldMethods/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TextFieldMethods
+//
+//  Created by VijayNew on 14/05/20.
+//  Copyright © 2020 VijayNew. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
